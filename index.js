9732b1082eedf72f74a9f67ab66dd4f96896d147
Blaise Ndizeye <khalifablaise@gmail.com>
2022-11-29 06:42:03 +0200

    Export the app from index.js and update the vercel config json

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config()
 
-const http = require("http")
 const express = require("express")
 const cors = require("cors")
 
@@ -13,6 +12,8 @@ app.use("/", require("./api/index"))
 
 let PORT = process.env.PORT || 5000
 
-http.createServer(app).listen(PORT, () => {
+app.listen(PORT, () => {
   console.log("Contact Mail Server started Successfully...")
 })
+
+module.exports = app
